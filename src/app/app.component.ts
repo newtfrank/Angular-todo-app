c05a2cc2544f45abbbb6e8c54dabdfb75783e371
Newton <mymbugua@gmail.com>
2018-09-28 14:05:18 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  todoArray = []
+  //add item to array
+  addTodo(value) {
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+ // delete item from todo
+  deleteItem(todo) {
+    for(let i=0; i<= this.todoArray.length ; i++) {
+      if(todo == this.todoArray[i]) {
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
 }
